6dd978a5a6f6025532c08ee6400e7e12cdb6bf59
Marylyn <marylynuzoukwu@gmail.com>
2023-12-18 13:49:32 -0500

    Edited comments to clean up code & added some edits to README

--- Final Project.py
+++ Final Project.py
@@ -1,14 +1,11 @@
 #Final Project
-#Incorporate pandas & matplotlib, git and regular expressions 
-
-#add what is required to readme, check class powerpoint & files
-
-
+#Incorporate pandas, matplotlib, git and regular expressions, to analyze data from a csv table 
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import os
+
+
+#House Prices Analysis
 
 #loaded the path of the table as a csv file, read the file and then printed the table.
 house_prices_path = "C:/Users/Maryl/OneDrive/Desktop/Final Project/American_Housing_Data_20231209.csv"
@@ -22,8 +19,6 @@ house_prices.describe()
 #counts the number of houses that has beds that are less than 9
 #saved that data into a df
 df_house_price = house_prices[house_prices["Beds"]<9] 
-#house_prices["Beds"] represents all the values in the column. 
-#house_prices["Beds"]<9 considers the Beds column, will return True for where the beds is less than 9
 #house_prices[house_prices["Beds"]<9] table info (rows & coulums) where the beds is less than 9 
 
 #used a histogram to plot the df 
@@ -37,11 +32,15 @@ plt.show()
 df_prices_by_beds =df_house_price[["Beds","Price"]].groupby(["Beds"]).mean() 
 #values of index, put into a column. Because the beds count is an index in the df
 df_prices_by_beds["Bed_count"] = df_prices_by_beds.index 
+
 #plotted a bar graph for the bed count, (where rooms is less than 9) vs the average price of the houses 
 plt.bar(x="Bed_count", height="Price", data = df_prices_by_beds)   
 plt.show()
 print(df_prices_by_beds)
 
+
+
+#Spotify Artist Analysis 
 
 #loaded the path of a csv file, read the file, & then printed the table
 afro_spotify_path = "C:/Users/Maryl/OneDrive/Desktop/Final Project/spotifyafro.csv"
@@ -49,12 +48,15 @@ afro_spotify = pd.read_csv(afro_spotify_path)
 print(afro_spotify)
 
 
-#Regular expression & indexing to print out the row of data where "boy" shows up in the artist name
+#Regular expression & indexing to print out the row of data where "Boy" or "boy" shows up in the artist name
 #/b == "word boundary"
 # [] only one option, lower or uppercase b
-artist_pattern = r".*[bB]oy/b" #matches anything that has boy or Boy .* = matches anything
+#matches anything that has boy or Boy .* = matches anything
+artist_pattern = r".*[bB]oy\b" 
 
-Boy_artist_name = afro_spotify[afro_spotify["artist"].str.match(artist_pattern)] #column/series
-# afro_spotify.loc[(afro_spotify["artist"] == "Burna Boy")]
+Boy_artist_name = afro_spotify[afro_spotify["artist"].str.match(artist_pattern)]
+#Displays the columns and rows for the artists that have "Boy" in "boy" in their name
 print(Boy_artist_name)
-Boy_artist_name.artist.unique() #returns the names of the artists that have boy in their name
+#returns the names of the artists that have "Boy" or "boy" in their name
+Boy_artist_name.artist.unique() 
+print(afro_spotify["artist"])
--- README.md
+++ README.md
@@ -1,37 +1,43 @@
 # INST126 Final Project 
 ## House Prices Analysis 
 Histogram for number of bedrooms vs number of houses 
-
+What Program Does:
 
 -Loads the path of a CSV file that has data on Houses in America. Then it reads the file and prints the data as a table.
 
 -Then provides the statistical analysis of the values in each coulumn of the table 
 
--Focuses on the number of bedrooms in the house, by looking at the columns and rows where the bedrooms is less than 9. Then count the number of houses that has bedrooms that are less than 9. Groups each count by the number of houses
+-Focuses on the number of bedrooms in the house, by looking at the columns and rows where the number of bedrooms is less than 9. Then counts the number of houses that has bedrooms that are less than 9. Groups each count by the number of bedrooms
 
--Saves that data into a dataframe & plots the data fram as a historgram
+-Saves that data into a dataframe & plots the data frame as a histogram
 
 
 Bar graph for number of bedrooms vs average price 
+What Program Does:
 
-
--Creates a dataframe that focuses on the housees where the total number of bedrooms that is less than 9.
+-Creates a dataframe that focuses on the houses where the total number of bedrooms that is less than 9.
 
 -Then it takes the average prices of the houses based on the number of bedrooms
 
 -Since the number of beds is an index in the dataframe, the index (bed count) was put into a column
 
--Plots a bar graph for the bedroomcount vs avergae price of the houses
+-Plots a bar graph for the bedroom count vs average prices of the houses
 
 
+*How to use Program:
+Run as as a script at the command line
 
 
-
-## Spotify Artists
+## Spotify Artist Analysis
+What Program Does:
 -Loads the path of a CSV file that has data on Afrobeat songs on Spotify. Then it reads the file and prints the data as table.
 
--Uses regular expressions and indexing to print out the roww of data were the word "Boy" or "boy" shows up in the artist name.
+-Uses regular expressions and indexing to print out the row of data where the word "Boy" or "boy" shows up in the artist name.
 
 -Then it prints out all the artists that have "Boy" or "boy" in their name into a column/series
 
--Then it retunrs just the names of all the artists that have "Boy" or "boy in their name.
+-Then it returns just the names of all the artists that have "Boy" or "boy in their name.
+
+
+*How to use program:
+Run as as a script at the command line
